eb7aa21b6c2fe38a78bac4accae8da9f20e73d40
D-Pagey <dan.page91@gmail.com>
2018-04-02 21:04:28 +0100

    Styling to the point where I need to inspect the real page.

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -7,8 +7,11 @@ function Title() {
     <div className="title-container">
       <h2 className='title'>Your credit score and report.</h2>
       <h2 className='title'>For free, forever.</h2>
-      <input type='text' name='emailInput' placeholder='Enter your email' 
-      className='email-input'/>
+      <div class='input-container'>
+        <input type='text' name='emailInput' placeholder='Enter your email' 
+        className='email-input'/>
+        <button className="email-btn">Get started</button>
+      </div>
     </div>
   )
 }
